842806578233cbd6b62e5552140ce481099e0bd8
PutriDwinatryska <90408123+PutriDwinatryska@users.noreply.github.com>
2022-04-13 04:42:45 +0700

    Add files via upload

---
+++ deret fibonaci1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int fibonacci (int n);
+
+int main()
+{
+ int angka,hasil;
+ cout<<"Menghitung fibonacci ke : "<<endl;
+ cin>>angka;
+ hasil=fibonacci(angka);
+ 
+ cout<<"nilainya adalah : ";
+ cout<<hasil<<endl;
+ cin.get();
+ return 0;
+}
+
+int fibonacci(int n)
+{
+ cout<<"fungsi fibonacci"<<n<<endl;
+ if((n==0)||(n==1))
+ {
+  return n;
+ }
+ else
+{
+ return fibonacci(n-1)+fibonacci(n-2);
+}
+}
